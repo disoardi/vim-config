b0628b95e3bef4d61b1e07a0c724a1da009cb20d
Davide Isoardi <davide.isoardi@valuepartners.com>
2019-03-04 17:18:09 +0100

    Modificato inserendo vundle e configurazioni per Python IDE

---
+++ setup_vim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp ./vimrc ~/.vim/vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+vim +PluginInstall +qall
+sudo dnf install cmake gcc-c++ make python3-devel
+python3 ~/.vim/bundle/install.py --all
+
